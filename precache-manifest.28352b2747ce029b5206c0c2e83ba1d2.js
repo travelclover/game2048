2df805f3e894b00a38dc1de74358421ab8c93620
dengdong <dengd@esrichina.com.cn>
2019-10-18 15:09:08 +0800

    更新

---
+++ precache-manifest.28352b2747ce029b5206c0c2e83ba1d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409c6c12e2fb31af8258ec01e2c423b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "953e9d4ddfb68be08626",
+    "url": "./static/css/main.6abffd52.chunk.css"
+  },
+  {
+    "revision": "bbea088ec8f6aad3f12d",
+    "url": "./static/js/2.d06edad6.chunk.js"
+  },
+  {
+    "revision": "953e9d4ddfb68be08626",
+    "url": "./static/js/main.4dbe805d.chunk.js"
+  },
+  {
+    "revision": "970ca2c6cf5565f20c61",
+    "url": "./static/js/runtime-main.f76a512f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae231989e97e176fb5e7f02ed95a49ba.js"
+  "./precache-manifest.28352b2747ce029b5206c0c2e83ba1d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
